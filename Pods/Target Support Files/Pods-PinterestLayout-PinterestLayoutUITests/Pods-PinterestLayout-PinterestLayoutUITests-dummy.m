8742b89ea9b741cf448cf2cf9c71fd8cf42b6317
Bắc Vương Toàn <43468251+vuongbac@users.noreply.github.com>
2020-08-31 11:41:59 +0700

    commit

---
+++ Pods/Target Support Files/Pods-PinterestLayout-PinterestLayoutUITests/Pods-PinterestLayout-PinterestLayoutUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PinterestLayout_PinterestLayoutUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PinterestLayout_PinterestLayoutUITests
+@end
